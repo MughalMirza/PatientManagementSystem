9d67bc6da2cfe8a43bc03008716294f03348a535
MughalMirza <tiger.mirza99@gmail.com>
2017-07-08 17:08:57 +0500

    Navigation Drawer

--- app/src/main/java/com/example/ahmad/pms/MainActivity.java
+++ app/src/main/java/com/example/ahmad/pms/MainActivity.java
@@ -90,9 +90,10 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
         }
+// else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
